b4785ecc3afb9c6ff5927c37c40e1b2b45744647
Lokeshprabu <aimprabu720@gmail.com>
2026-04-25 18:51:24 +0530

    LeetCode solutions

---
+++ 75.sort-colors.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=75 lang=python
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+# @lc code=end
+
